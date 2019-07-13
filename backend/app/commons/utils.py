519ddce1bc18422a272251ea2aa849845ad4c11a
paucar97 <c.paucarpoma@pucp.pe>
2019-07-13 16:45:48 -0500

    envio de correos sera con hilo

--- backend/app/commons/utils.py
+++ backend/app/commons/utils.py
@@ -1,5 +1,5 @@
 import smtplib
-
+import threading
 def convertDatetime(tiempo):
     tiempo = tiempo.__str__()
     tiempo = tiempo.replace("T"," ")
@@ -10,17 +10,22 @@ correoSistemaSec = "florestacksistemasec2@gmail.com"
 contrasenaSistemaSec = "florestack123"
 
 def envioCorreo(destinatarios,asunto,mensage):
-    try:
-        me = 'Subject: {}\n\n{}'.format(asunto,mensage)
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.starttls()
-        server.login(correoSistemaSec,contrasenaSistemaSec)
-        server.sendmail(correoSistemaSec,destinatarios,me)
-        server.quit()
-        print("ENVIO CORREOS")
-    except:
-        print("NO ENVIO CORREOS")
+    def funcion():
+        try:
+            me = 'Subject: {}\n\n{}'.format(asunto,mensage)
+            server = smtplib.SMTP('smtp.gmail.com',587)
+            server.starttls()
+            server.login(correoSistemaSec,contrasenaSistemaSec)
+            server.sendmail(correoSistemaSec,destinatarios,me)
+            server.quit()
+            print("ENVIO CORREOS")
+        except:
+            print("NO ENVIO CORREOS")
         return
+
+    x = threading.Thread(target=funcion)
+    x.start()
+
     return
 
 
